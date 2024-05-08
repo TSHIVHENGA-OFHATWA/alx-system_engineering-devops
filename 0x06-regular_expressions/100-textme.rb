8ef15852a46b24bab4df91b05dd54cce8f945870
OFHATWA <tofhatwa@gmail.com>
2024-05-08 04:25:12 +0200

    100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-#This script scan for
+#This script scan for TextMe app text messages transactions.
 
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flag:(.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\]\[to:(.*?)\]\[flag:(.*?)\]/).join(',')
