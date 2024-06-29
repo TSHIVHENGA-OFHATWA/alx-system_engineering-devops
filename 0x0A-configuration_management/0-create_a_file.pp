5a232eb20faea01b4ab1c360e7c426ae8e76d4d5
ofhatwa <tofhatwa@gmail.com>
2024-06-30 01:26:04 +0200

    1-install_a_package.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,3 +1,6 @@
+#Puppet manifest to create a file in /tmp/school
+
+# Define the file resource for /tmp/school
 file { '/tmp/school':
   ensure  => file,
   mode    => '0744',
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,12 @@
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  unless  => '/usr/bin/pip3 show flask | grep -q "^Version: 2.1.0$"',
+  require => Package['python3-pip'],
+}
