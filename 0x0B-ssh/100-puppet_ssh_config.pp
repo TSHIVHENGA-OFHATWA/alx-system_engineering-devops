f5cf4efb92d0fbd07dae4e88ec0618d0caebc357
ofhatwa <tofhatwa@gmail.com>
2024-07-01 20:22:03 +0200

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,32 +1,6 @@
-# 100-puppet_ssh_config.pp
-
-# Ensure the .ssh directory exists
-file { '/home/vagrant/.ssh':
-  ensure => directory,
-  owner  => 'vagrant',
-  group  => 'vagrant',
-  mode   => '0700',
-}
-
-# Ensure the SSH client configuration file exists
-file { '/home/vagrant/.ssh/config':
-  ensure  => file,
-  owner   => 'vagrant',
-  group   => 'vagrant',
-  mode    => '0600',
-  content => '',
-}
-
-# Disable password authentication
-file_line { 'Turn off passwd auth':
-  path  => '/home/vagrant/.ssh/config',
-  line  => 'PasswordAuthentication no',
-  match => '^#?PasswordAuthentication',
-}
-
-# Declare the identity file
-file_line { 'Declare identity file':
-  path  => '/home/vagrant/.ssh/config',
-  line  => 'IdentityFile ~/.ssh/school',
-  match => '^#?IdentityFile',
-}
+# This is my ubuntu sever for 237907-web-01
+Host my_server
+    HostName 34.224.95.251
+    User ubuntu
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
