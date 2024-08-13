25e01a503dcec3a6a7a58dde2ee6d88ef90ee136
ofhatwa <tofhatwa@gmail.com>
2024-08-13 21:35:24 +0200

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" queries the Reddit API and returns the number of subscribers."""
+"""queries the Reddit API and returns the number of subscribers."""
+
 
 import requests
 
@@ -14,7 +15,7 @@ def number_of_subscribers(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
