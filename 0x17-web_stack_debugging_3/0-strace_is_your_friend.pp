0f16f3037e3e81378f7f6d711481be6328257977
ofhatwa <tofhatwa@gmail.com>
2024-08-14 21:25:46 +0200

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix the phpp extension in the wp-setting
 exec { 'fix-wp-locale':
   command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php"
-  path    =>  ['/bin', '/usr/local/bin']
+  path    =>  ['/bin', '/usr/bin']
 }
